aad6e4e4023ef8c4280f9493a700d15d3fa6f61a
Anirudh <cidirianirudh@gmail.com>
2023-05-29 22:12:44 +0530

    Completed commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./style.css";
+import React, { useEffect, useState, useRef } from "react";
 
-function App() {
+const App = () => {
+  const [quotes, setQuotes] = useState("");
+  const textRef = useRef();
+  let colors = ["#ffff00", "#90ee90", "#ffa500", "#ff68ff", "#a9a9e7"];
+  const getQuote = () => {
+    fetch("https://type.fit/api/quotes")
+      .then((res) => res.json())
+      .then((data) => {
+        let randomNum = Math.floor(Math.random() * data.length);
+        setQuotes(data[randomNum]);
+        console.log(data[randomNum]);
+      })
+      .catch((error) => console.log(error));
+  };
+  useEffect(() => {
+    getQuote();
+  }, []);
+
+  useEffect(() => {
+    textRef.current.style.color =
+      colors[Math.floor(Math.random() * colors.length)];
+  }, [quotes]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="quote">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <h3 ref={textRef}>{quotes.text}</h3>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>
+          <h3>Author: {quotes.author}</h3>
+        </p>
+        <div className="btnContainer">
+          <button onClick={getQuote} className="btn">
+            Get Quotes
+          </button>
+          <a
+            href={`https://twitter.com/intent/tweet?text=${quotes.text}`}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="btn"
+          >
+            Tweet
+          </a>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
